5e0cba79880c34751af65abfcb363656623f03dc
Kazuma <kazuma@example.com>
2019-07-14 11:00:45 +0900

    削除機能まで完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    
+  end
+ 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "ブログを登録しました。"
+    else
+      render :new 
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "ブログを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "ブログを削除しました。"
+  end
+
+private
+
+def post_params
+    params.require(:post).permit(:title, :body, :category)
+end
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 50}
+  validates :body, presence: true, length: {maximum: 2000}
+
+
+
 end
